ed811ff20316963035883edcc3c89a91185fc2e6
Dave <DaveBathnes@users.noreply.github.com>
2019-08-26 14:36:34 +0100

    Updates

---
+++ precache-manifest.f94ecb71fd49bf5e64180836fb6b051e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4711036d00aece45d7d1f05855458ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1194f3bd6238a93a3ad8",
+    "url": "/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "1194f3bd6238a93a3ad8",
+    "url": "/static/js/2.71963e1e.chunk.js"
+  },
+  {
+    "revision": "1358870f9200670518c0",
+    "url": "/static/js/main.339330a3.chunk.js"
+  },
+  {
+    "revision": "69e6583775b1a13e34df",
+    "url": "/static/js/runtime~main.4c74b9cd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb661c77e5b1bed98a409ff690334605.js"
+  "/precache-manifest.f94ecb71fd49bf5e64180836fb6b051e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
