431c1314e1752e67d2bf20109e86b9a6aba852bf
Dave <DaveBathnes@users.noreply.github.com>
2019-10-11 00:01:43 +0100

    Updates

---
+++ precache-manifest.9f2bc22e0fa736aca6c77017d0ab8e8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b300b87743c8b7e74ea7af79df1e8c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "081a4129fe397a20724a",
+    "url": "/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "081a4129fe397a20724a",
+    "url": "/static/js/2.94242027.chunk.js"
+  },
+  {
+    "revision": "5cc98109f7dce22ed153",
+    "url": "/static/js/main.11706f63.chunk.js"
+  },
+  {
+    "revision": "3d39fa46d8913c04a532",
+    "url": "/static/js/runtime-main.87c12a3f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d9a5200da1ec3b1b6fea138efeba8f0.js"
+  "/precache-manifest.9f2bc22e0fa736aca6c77017d0ab8e8b.js"
 );
 
 self.addEventListener('message', (event) => {
